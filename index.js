b0828dfbc701b417679aadd87063afeae98e2f56
duy.tranngoc <duy.tranngoc@haravan.com>
2018-03-27 21:36:41 +0700

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var server = require('http').Server(app);
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 
 app.get('/', function (res, req) {
     req.send('Trang test scan QR code, nhưng không có gì ở đây hết :D ');
