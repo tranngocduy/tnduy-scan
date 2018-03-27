3eada01973d222f7300cf2baacea301b9389a71c
duy.tranngoc <duy.tranngoc@haravan.com>
2018-03-27 22:07:14 +0700

    aaaa

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ app.get('/', function (res, req) {
     req.send('Trang test scan QR code, nhưng không có gì ở đây hết :D ');
 })
 
+app.get('/hr-qrcode', function (res, req) {
+    try {
+        req.sendFile(__dirname + '/index.html');
+    } catch (error) {
+        console.log('error', error)
+    }
+})
+
