5507f3aa36a446936bf194635e0e8246dc45c799
duy.tranngoc <duy.tranngoc@haravan.com>
2018-03-27 21:56:49 +0700

    aaa

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.port || 3000;
+
+app.listen(port);
+
 app.get('/', function (res, req) {
     req.send('Trang test scan QR code, nhưng không có gì ở đây hết :D ');
 })
 
-var port = process.env.port || 3000;
-
-app.listen(port);
